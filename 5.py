e12de83c12dfe012a3ad5f115bfd5148cdaea265
tarunbodapati <36191528+tarunbodapati@users.noreply.github.com>
2019-07-17 18:19:34 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,6 @@
+g1=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+g2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+r=str(input())
+for j in range(20):
+    if(r==g1[j]):
+        print(g2[j])
